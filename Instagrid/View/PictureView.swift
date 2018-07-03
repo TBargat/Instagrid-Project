698d0712ed0c4b1050288ea5403f0df36f00f7c7
TBargat <tbargat@gmail.com>
2018-07-03 10:40:07 +0200

    Creation of the Supporting Files and the structure of the project.

---
+++ Instagrid/View/PictureView.swift
@@ -0,0 +1,21 @@
+//
+//  PictureView.swift
+//  Instagrid
+//
+//  Created by Thibault Dev on 03/07/2018.
+//  Copyright © 2018 Thibault Dev. All rights reserved.
+//
+
+import UIKit
+
+class PictureView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
